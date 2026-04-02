de6f6a924613918053ff1353e295a885f3ddfdea
Parisha Sharma <191549341+parisha-sharma@users.noreply.github.com>
2026-04-02 23:24:35 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
-def get_connection():
-    connection = mysql.connector.connect(
+def get_connection():    #Creating a function to connect to SQL
+    password = input("Enter your DB password: ")    #input password from the user
+    connection = mysql.connector.connect(    #connecting with SQL
         host="localhost",
-        user="root",         
-        password="Pari1503", 
-        database="expense_tracker"
+        user="root",
+        password=password,
+        database="expense_tracker"    #Database name
     )
     return connection
